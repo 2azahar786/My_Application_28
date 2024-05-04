70b5bbf2eb357499460df51fb681d145293807a5
2azahar786.2 <2azahar786.2@gmail.com>
2024-05-05 02:44:33 +0530

    initial commit

---
+++ src/Form.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Form = () => {
+    return(
+        <div className= 'form'>
+           
+            <form action='submit'>
+            <h1> Hello,  Mr. Azhar</h1>
+            <h1>How are you Azhar?</h1>
+            <label for="name">Name:</label> 
+
+
+            </form>
+        </div>
+    )
+}
+
+export default Form;
